5dd87a85ae89b6a2b304058b915dfb8db8359a0e
Jeff Aigner <jeff.aigner@gmail.com>
2026-05-10 07:04:59 -0500

    update, remove x11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NAS server config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = {self, nixpkgs }: {
